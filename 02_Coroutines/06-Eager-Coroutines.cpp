ca451935a40efeb3b2083b1771fbe9e554f2895e
Gajendra <9218964+ggulgulia@users.noreply.github.com>
2022-05-07 14:58:33 +0200

    updates
    1. simplify returnobject
    2. use coroutine_handle and alias to handle

--- 02_Coroutines/06-Eager-Coroutines.cpp
+++ 02_Coroutines/06-Eager-Coroutines.cpp
@@ -8,13 +8,10 @@
 
 struct EagerTask {
         struct promise_type {
-            int val_;
-            EagerTask get_return_object() {
-                return {std::coroutine_handle<promise_type>::from_promise(*this)}; 
-            }
 
             std::suspend_never initial_suspend() { return {}; }
-
+            
+            EagerTask get_return_object() {return {std::coroutine_handle<promise_type>::from_promise(*this)}; }
             std::suspend_always final_suspend() noexcept { return {}; }
             void unhandled_exception() {}
         };
@@ -23,25 +20,21 @@ struct EagerTask {
       EagerTask(std::coroutine_handle<promise_type> h):h_{h}{ } 
       operator std::coroutine_handle<promise_type>() const { return h_; }
       
-      void resume(){
-          if(!h_.done()){
-              h_();
-          }
-      }
 };
 
 
-EagerTask lazy_coroutine()
+EagerTask eager_coroutine()
 {
     std::cout << "1. lazy_coroutine started\n";
     co_await std::suspend_always{};
     std::cout << "2. lazy_coroutine finished\n";
 }
 
+using Task = std::coroutine_handle<EagerTask::promise_type>;
 
 int main(){
 
-    EagerTask task = lazy_coroutine();
-    task.resume();
-
+    Task task = eager_coroutine();
+    std::cout << "control returned to main function\n";
+    task();
 }
